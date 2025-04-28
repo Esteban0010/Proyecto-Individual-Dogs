ca34f808499ec9320bce262519cc8e1e09f9ed79
Esteban0010 <estebanpilchuman02@gmail.com>
2025-04-27 22:57:16 -0300

    AA

--- api/src/app.js
+++ api/src/app.js
@@ -1,33 +1,44 @@
 const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
-require('./db.js');
+ const cors = require('cors');
+ const cookieParser = require('cookie-parser');
+ const bodyParser = require('body-parser');
+ const morgan = require('morgan');
+ const routes = require('./routes/index.js');
+ require('./db.js');
+ 
+ const server = express();
 
-const app = express();
-
-app.use(cors({
-  origin: 'https://proyecto-individual-dogs-nu.vercel.app', // Permitir solicitudes solo desde tu frontend
-  credentials: true, // Permitir cookies si es necesario
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Headers permitidos
-}));
-
-// Tu otra configuración de middlewares
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cookieParser());
-app.use(morgan('dev'));
-app.use('/', routes);
-
-// Manejo de errores
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-module.exports = app;
+ 
+ // Habilitar CORS para solicitudes desde tu frontend
+ server.use(cors({
+   origin: 'https://proyecto-individual-dogs-nu.vercel.app', // Permitir solicitudes solo de tu frontend
+   credentials: true, // Si estás usando cookies o headers especiales
+ }));
+ 
+ server.name = `API`;
+ 
+ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+ server.use(bodyParser.json({ limit: '50mb' }));
+ server.use(cookieParser());
+ server.use(morgan('dev'));
+ server.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'https://proyecto-individual-dogs-nu.vercel.app'); // update to match the domain you will make the request from
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+   next();
+ });
+ 
+ // Rutas
+ server.use('/', routes);
+ 
+ // Error catching endware.
+ // Error catching endware
+ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+   const status = err.status || 500;
+   const message = err.message || err;
+   console.error(err);
+   res.status(status).send(message);
+ });
+ 
+ module.exports = server;
